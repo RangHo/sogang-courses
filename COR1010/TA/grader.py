c865415bcaa69cb5170bd7160c72dd549e372108
RangHo Lee <hello@rangho.me>
2023-04-23 21:12:01 +0900

    [COR1010] Add more features to the grading script Now one can strip or ignore whitespaces when grading, and extract the result as diff files for easy grading.

--- COR1010/TA/grader.py
+++ COR1010/TA/grader.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
+# Module imports
 import sys
 
 import argparse
 import csv
+import difflib
 import subprocess
 import re
 
 from pathlib import Path
 
-from typing import Optional
+# Type imports
+from typing import Optional, Final
 
 # List of encodings to try before tapping out
-POSSIBLE_ENCODINGS = [
+POSSIBLE_ENCODINGS: Final = [
     sys.stdout.encoding or sys.getdefaultencoding(),
     sys.stdin.encoding or sys.getdefaultencoding(),
     'euc-kr',
@@ -25,7 +28,63 @@ POSSIBLE_ENCODINGS = [
 #  - 0: No logging
 #  - 1: Informational info
 #  - 2: Debugging info
-LOG_LEVEL = 1
+LOG_LEVEL: int = 1
+
+class RunResult:
+    """Result of a program."""
+
+    IGNORE_WHITESPACE = False
+    STRIP_WHITESPACE = False
+
+    def __init__(self, target: str, input: Optional[str], success: bool, output: str):
+        self.target = target
+        self.input = input if input is not None else "N/A"
+        self.success = success
+        self.output = output.splitlines(keepends=True)
+
+    def __eq__(self, other):
+        """Compare two results."""
+        if isinstance(other, str):
+            return self.output == other.splitlines(keepends=True)
+
+        if not self.success or not other.success:
+            return False
+
+        # Strip whitespace for each line if needed
+        self_output = [line.strip() for line in self.output] \
+            if self.STRIP_WHITESPACE else self.output
+        other_output = [line.strip() for line in other.output] \
+            if other.STRIP_WHITESPACE else other.output
+
+        # Ignore whitespace if needed
+        self_output = [line.replace(" ", "") for line in self_output] \
+            if self.IGNORE_WHITESPACE else self_output
+        other_output = [line.replace(" ", "") for line in other_output] \
+            if other.IGNORE_WHITESPACE else other_output
+
+        return self_output == other_output
+
+    def __ne__(self, other):
+        """Compare two results."""
+        return not self.__eq__(other)
+
+    def __str__(self):
+        """Return a string representation of the result."""
+        return "".join(self.output)
+
+    def diff(self, other) -> str:
+        """Return a diff of the output of two results."""
+        result = difflib.unified_diff(
+            self.output,
+            other.output,
+            fromfile=self.target,
+            tofile=other.target,
+        )
+        return "".join(result)
+
+
+def log_error(*args, **kwargs):
+    print("[ERROR]", *args, **kwargs)
 
 
 def log_info(*args, **kwargs):
@@ -78,23 +137,42 @@ def run_with_input(script_path: Path, input_path: Path) -> str:
     return try_decode(output)
 
 
-def run(script_path: Path, input_path: Optional[Path] = None) -> tuple[bool, str]:
+def run(script_path: Path, input_path: Optional[Path] = None) -> RunResult:
     """Run a script and return its output."""
 
     # Of course, their code can set this computer on fire
     try:
+        result = None
         if input_path is None:
-            return True, run_without_input(script_path)
+            result = run_without_input(script_path)
+            return RunResult(
+                target=script_path.name,
+                input=None,
+                success=True,
+                output=result
+            )
         else:
-            return True, run_with_input(script_path, input_path)
+            result = run_with_input(script_path, input_path)
+            return RunResult(
+                target=script_path.name,
+                input=input_path.read_text(),
+                success=True,
+                output=result
+            )
+
     except subprocess.CalledProcessError as e:
         # Oh no who would've guessed
         error_output = try_decode(e.output)
 
         log_info("Student's code failed to run!")
         log_info("Output:", error_output)
-        
-        return False, error_output
+
+        return RunResult(
+            target=script_path.name,
+            input=input_path.read_text() if input_path is not None else None,
+            success=False,
+            output=error_output
+        )
 
 
 if __name__ == '__main__':
@@ -121,6 +199,18 @@ if __name__ == '__main__':
         nargs='*',
         help='The verbatim text file that contains the answer'
     )
+    parser.add_argument(
+        '--ignore-whitespace',
+        action='store_true',
+        default=False,
+        help='Ignore whitespace when comparing the output of the submitted code with the answer'
+    )
+    parser.add_argument(
+        '--strip-whitespace',
+        action='store_true',
+        default=False,
+        help='Strip whitespace from the output of the submitted code before comparing it with the answer'
+    )
 
     # Arguments related to information gathering
     parser.add_argument(
@@ -134,12 +224,18 @@ if __name__ == '__main__':
         help='The regex to filter out files that should not be graded'
     )
 
-    # Enable CSV output?
+    # Arguments related to the result output
     parser.add_argument(
         '--csv-out',
         nargs='?',
         const='output.csv',
         help='The file name of the CSV file containing the result'
+    )
+    parser.add_argument(
+        '--diff-out',
+        nargs='?',
+        const='diff',
+        help='The directory name of the diff files'
     )
 
     # How talkative should I be?
@@ -209,6 +305,10 @@ if __name__ == '__main__':
         if len(args.testcase) != len(args.answer_text):
             raise ValueError("The number of test cases must match the number of answer text files.")
 
+    # Set whitespace flags
+    RunResult.IGNORE_WHITESPACE = args.ignore_whitespace
+    RunResult.STRIP_WHITESPACE = args.strip_whitespace
+
     # Convert the list of test cases into a list of Path objects
     # If no testcases are available then return a list of None
     testcase_paths = [Path(testcase) for testcase in args.testcase] \
@@ -220,15 +320,15 @@ if __name__ == '__main__':
     answer_list = []
     if args.answer_code is not None:
         for testcase_path in testcase_paths:
-            normal_exit, answer_output = run(Path(args.answer_code), testcase_path)
-            answer_list.append(answer_output)
+            answer_result = run(Path(args.answer_code), testcase_path)
+            answer_list.append(answer_result)
     else:
         for answer_text in args.answer_text:
             with open(answer_text, 'r') as f:
                 answer_list.append(f.read())
 
     # Set up a list to store results
-    results = [['Student ID', 'Filename', 'Student Output', 'Correct Output', 'Result']]
+    results: list[dict] = []
 
     # Find all student code
     student_code_paths = Path(args.directory).glob('*.py')
@@ -245,26 +345,56 @@ if __name__ == '__main__':
         for testcase, answer in zip(testcase_paths, answer_list):
             # Gathering some info before running the code
             student_id = re.search(args.id_regex, student_code.name).group(0)
-            
-            normal_exit, student_output = run(student_code, testcase)
+            student_result = run(student_code, testcase)
 
-            results.append([
-                student_id,
-                student_code.name,
-                'Failed to run' if not normal_exit else student_output,
-                answer,
-                'correct' if student_output == answer else 'incorrect'
-            ])
+            results.append({
+                'id': student_id,
+                'code': student_code.name,
+                'testcase': testcase.name if testcase is not None else None,
+                'success': student_result.success,
+                'correct': answer == student_result,
+                'answer_result': answer,
+                'student_result': student_result,
+            })
 
-    # Print the result just in case...
-    for row in results:
-        log_info(f"{row[0]}'s code in {row[1]} is {row[4]}.")
+    # Print the result first
+    for item in results:
+        log_info(item)
 
-    # ...and save it if the user asked to do so
-    if args.csv_out is not None:
-        log_info("Writing to a CSV file: " + args.csv_out)
+    # Save it as CSV if the user asked to do so
+    try:
+        if args.csv_out is not None:
+            log_info("Writing to a CSV file:", args.csv_out)
 
-        # Add BOM as Excel doesn't like CSV files without metadata
-        with open(args.csv_out, 'w', encoding='utf-8-sig') as f:
-            writer = csv.writer(f)
-            writer.writerows([s.replace('\n', '\\n') for s in row] for row in results)
+            # Add BOM as Excel doesn't like CSV files without metadata
+            with open(args.csv_out, 'w', encoding='utf-8-sig') as f:
+                writer = csv.writer(f)
+                writer.writerow(results[0].keys())
+                writer.writerows(
+                    [str(value).replace('\n', '\\n') for _, value in item.items()]
+                    for item in results
+                )
+    except Exception as e:
+        log_error("Failed to write to a CSV file:", e)
+
+    # Save the diff files if the user asked to do so
+    try:
+        if args.diff_out is not None:
+            log_info("Writing diff files to directory:", args.diff_out)
+
+            # Create the directory if it doesn't exist
+            Path(args.diff_out).mkdir(parents=True, exist_ok=True)
+
+            # Filter out correct students
+            incorrect_results = [item for item in results if not item['correct']]
+
+            # Start processing
+            for item in incorrect_results:
+                # Create a diff file for each incorrect student
+                diff_file = Path(args.diff_out) / f"{item['id']}.diff"
+                with open(diff_file, 'w') as f:
+                    # Print the diff result to the file
+                    log_debug(f"Writing diff file for student {item['id']}...")
+                    f.write(item['answer_result'].diff(item['student_result']))
+    except Exception as e:
+        log_error("Failed to write to the diff file:", e)
