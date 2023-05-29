7cc9abded43013526006a3c59d3a1772236aa433
RangHo Lee <hello@rangho.me>
2023-05-30 02:21:31 +0900

    [CSE4100] Start task1 implementation

--- CSE4100/Project2/task1/stockclient.c
+++ CSE4100/Project2/task1/stockclient.c
@@ -4,15 +4,15 @@
 /* $begin echoclientmain */
 #include "csapp.h"
 
-int main(int argc, char **argv) 
+int main(int argc, char** argv)
 {
     int clientfd;
     char *host, *port, buf[MAXLINE];
     rio_t rio;
 
     if (argc != 3) {
-	fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
-	exit(0);
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
     }
     host = argv[1];
     port = argv[2];
@@ -21,11 +21,11 @@ int main(int argc, char **argv)
     Rio_readinitb(&rio, clientfd);
 
     while (Fgets(buf, MAXLINE, stdin) != NULL) {
-	Rio_writen(clientfd, buf, strlen(buf));
-	Rio_readlineb(&rio, buf, MAXLINE);
-	Fputs(buf, stdout);
+        Rio_writen(clientfd, buf, strlen(buf));
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf, stdout);
     }
-    Close(clientfd); //line:netp:echoclient:close
+    Close(clientfd); // line:netp:echoclient:close
     exit(0);
 }
 /* $end echoclientmain */
